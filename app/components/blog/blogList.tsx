3beb3db9222b7f1a4f070b2271e4345cc4d12f20
Jeremy Hill <jhill83@gmail.com>
2024-06-20 22:51:58 -0500

    additional test

--- app/components/blog/blogList.tsx
+++ app/components/blog/blogList.tsx
@@ -1,14 +1,23 @@
 import { BlogListProps, BlogListFilter } from '@/types/BlogListProps'
 import { BlogPost } from '@/types/BlogPost'
 import { getPosts } from '@/lib/sanityClient'
+import BlogItem from './blogItem'
 
 export default async function BlogList({ count = null, filter = {} as BlogListFilter }: BlogListProps) {
   const posts = await getPosts(count, filter) as Array<BlogPost>
-  console.log(posts)
 
   return (
     <div className="w-full flex flex-col justify-items-start content-center flex-wrap">
-      <ul />
+      <ul>
+        {posts.length > 0
+        && posts.map((post: BlogPost, index: number) => (
+          <BlogItem
+            blogPost={ post as BlogPost }
+            side={ index % 2 === 0 ? 'left' : 'right' }
+            key={ post?.slug?.current }
+          />
+        ))}
+      </ul>
     </div>
   )
 }
