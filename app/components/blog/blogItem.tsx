49ea4f27375ab9853d2465a21c84e563eacd0aa8
Jeremy Hill <jhill83@gmail.com>
2024-06-20 22:58:40 -0500

    testing

--- app/components/blog/blogItem.tsx
+++ app/components/blog/blogItem.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image'
 import { BlogItemProps } from '@/types/BlogItemProps'
+import Tag from '@/components/blog/tag'
+import { urlFor } from '@/lib/portableTextUtils'
+import Link from 'next/link'
 
 export default function BlogItem({ blogPost, side = 'left' }: BlogItemProps) {
   return (
-    <li className={ `w-full relative over p-4 mb-8 flex flex-col md:flex-row drop-shadow-md blog-item-${ blogPost.category.name.current } ${ side === 'left' ? '' : 'md:justify-end' }` } />
+    <li className={ `w-full relative over p-4 mb-8 flex flex-col md:flex-row drop-shadow-md blog-item-${ blogPost.category.name.current } ${ side === 'left' ? '' : 'md:justify-end' }` }>
+      <Image
+        fill
+        src={ urlFor(blogPost.mainImage).width(1500).height(500).url() }
+        className="md:object-cover object-contain md:inline-block hidden"
+        alt={ blogPost.mainImage.alt }
+      />
+      <Image
+        height={ 1500 }
+        width={ 1500 }
+        src={ urlFor(blogPost.mainImage).width(1500).height(1500).url() }
+        className="md:object-cover object-contain md:hidden absolute top-0 left-0"
+        alt={ blogPost.mainImage.alt }
+      />
+      <article className="md:w-2/5 2xl:w-1/5 relative text-white p-4 md:-mt-8 mt-44 drop-shadow-2xl" />
+    </li>
   )
 }
