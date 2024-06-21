a190cd6e7e749033c797ff0dcebce20df5d25fa7
Jeremy Hill <jhill83@gmail.com>
2024-06-20 21:18:37 -0500

    re add bloglist

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,12 @@
+import BlogList from '@/components/blog/blogList'
+
 export default function Blog() {
   return (
     <main className="container mx-auto w-full">
       <h1 className="text-center mt-6 mb-6 font-bold text-4xl font-heading">
         Musings
       </h1>
+      <BlogList count={ 4 } />
     </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/hero'
+import BlogList from './components/blog/blogList'
 
 export default function Home() {
   return (
     <main className="container mx-auto w-full">
       <Hero />
+      <BlogList count={ 4 } />
     </main>
   )
 }
