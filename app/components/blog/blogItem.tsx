c161e658137ba965595e917dc098462263e2e4c1
Jeremy Hill <jhill83@gmail.com>
2024-06-20 23:06:12 -0500

    typescript

--- app/components/blog/blogItem.tsx
+++ app/components/blog/blogItem.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import { BlogItemProps } from '@/types/BlogItemProps'
-import Tag from '@/components/blog/tag'
 import { urlFor } from '@/lib/portableTextUtils'
 import Link from 'next/link'
 
