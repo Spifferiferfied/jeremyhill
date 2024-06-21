af12cd23b8134e55c8ef4a169c971c9d7874278a
Jeremy Hill <jhill83@gmail.com>
2024-06-20 23:03:57 -0500

    without tag

--- app/components/blog/blogItem.tsx
+++ app/components/blog/blogItem.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import { BlogItemProps } from '@/types/BlogItemProps'
+import Tag from '@/components/blog/tag'
 import { urlFor } from '@/lib/portableTextUtils'
+import Link from 'next/link'
 
 export default function BlogItem({ blogPost, side = 'left' }: BlogItemProps) {
   return (
-    <li className={ `w-full relative over p-4 mb-8 flex flex-col md:flex-row drop-shadow-md blog-item-${ blogPost.category.name.current } ${ side === 'left' ? '' : 'md:justify-end' }` }>
+    <li
+      className={ `w-full relative over p-4 mb-8 flex flex-col md:flex-row drop-shadow-md blog-item-${ blogPost.category.name.current } ${ side === 'left' ? '' : 'md:justify-end' }` }
+    >
       <Image
         fill
         src={ urlFor(blogPost.mainImage).width(1500).height(500).url() }
@@ -18,7 +22,25 @@ export default function BlogItem({ blogPost, side = 'left' }: BlogItemProps) {
         className="md:object-cover object-contain md:hidden absolute top-0 left-0"
         alt={ blogPost.mainImage.alt }
       />
-      <article className="md:w-2/5 2xl:w-1/5 relative text-white p-4 md:-mt-8 mt-44 drop-shadow-2xl" />
+      <article className="md:w-2/5 2xl:w-1/5 relative text-white p-4 md:-mt-8 mt-44 drop-shadow-2xl">
+        <h2 className="font-heading text-3xl font-bold mb-2">
+          <Link
+            href={ `/blog/${ blogPost.slug.current }` }
+            className="text-white no-underline hover:underline"
+          >
+            { blogPost.title }
+          </Link>
+        </h2>
+        <div className="leading-6 mb-4">
+          { blogPost.blurb }
+        </div>
+        <Link
+          className="text-sm inline-block px-2 py-1 read-more float-right hover:underline no-underline text-white"
+          href={ `/blog/${ blogPost.slug.current }` }
+        >
+          Read More...
+        </Link>
+      </article>
     </li>
   )
 }
