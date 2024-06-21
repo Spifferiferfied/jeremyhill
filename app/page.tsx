ce5045c95697396e88da802837b18f6f6fcd249b
Jeremy Hill <jhill83@gmail.com>
2024-06-20 21:04:10 -0500

    testing more

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from '@/components/hero'
-import BlogList from './components/blog/blogList'
 
 export default function Home() {
   return (
     <main className="container mx-auto w-full">
       <Hero />
-      <BlogList count={ 4 } />
     </main>
   )
 }
