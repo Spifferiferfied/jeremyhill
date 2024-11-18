7cb6f861a9ac1b794dd061f0a150ab32dd633c36
Jeremy Hill <jhill83@gmail.com>
2024-11-18 12:58:23 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export function middleware(req: NextRequest) {
   const currentEnv = process.env.NODE_ENV as Environment
   const isHttps = headers.get('x-forwarded-proto')?.split(',')[0] === 'https'
   const isLocalhost = req.headers.get('host')?.includes('localhost')
-  if (currentEnv === 'production' && isHttps && !isLocalhost) {
+  if (currentEnv === 'production' && !isHttps && !isLocalhost) {
     const newUrl = new URL(`http://${ headers.get('host') }` || '')
     newUrl.protocol = 'https:'
     return NextResponse.redirect(newUrl.href, 301)
