69b08188a26cce23d79f998e7c5da9f5d7af6d07
Jeremy Hill <jhill83@gmail.com>
2025-02-07 20:54:56 -0500

    dialog role

--- app/components/gallery/galleryImage.tsx
+++ app/components/gallery/galleryImage.tsx
@@ -14,7 +14,7 @@ export default function GalleryImage({ image }: { image: SanityGalleryImage }) {
     ref.current?.close()
   }
   const dialogEnter = (e: KeyboardEvent<HTMLElement>) => {
-    if (e.key === 'Enter'){
+    if (e.key === 'Enter') {
       closeDialog()
     }
   }
@@ -28,7 +28,7 @@ export default function GalleryImage({ image }: { image: SanityGalleryImage }) {
         width={ 500 }
         onClick={ openDialog }
       />
-      <dialog ref={ ref } className="fixed" onClick={ closeDialog } onKeyDown={ dialogEnter } role="button" tabIndex={ 0 }>
+      <dialog ref={ ref } className="fixed" onClick={ closeDialog } onKeyDown={ dialogEnter } role="dialog" tabIndex={ 0 }>
         <Image
           src={ urlFor(image).dpr(2).url() }
           alt={ image.alt }
